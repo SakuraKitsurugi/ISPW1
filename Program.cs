7e1ca30bf4ab7d525004b32f2ec3217ca0347925
Sakura <sakura.kitsurugi@proton.me>
2025-03-10 11:25:48 +0200

    -

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+					
+public class ISPW1
+{
+	public static void Main()
+	{
+		Console.WriteLine("Input text:\n");
+		string inputText = Console.ReadLine();
+		Console.Clear();
+		Console.WriteLine("Input key:\n");
+		string inputKey = Console.ReadLine();
+		Console.Clear();
+		
+		Console.WriteLine("Text: " + inputText + "\n");
+		Console.WriteLine("Key " + inputKey + "\n");
+		Console.WriteLine("Encrypted with ASCII: " + EncryptASCII(inputText, inputKey));
+		Console.WriteLine("Decrypted with ASCII: " + DecryptASCII(inputText, inputKey));
+	}
+	
+	public static string EncryptASCII(string text, string key){
+		string encryptedText = "";
+		
+		for(int i = 0; i < text.Length; i++){
+			int textLetter = (int)text[i];
+			int keyLetter = (int)key[i % key.Length];
+			int encryptedLetter = (textLetter + keyLetter) % 127;
+			if(encryptedLetter < 32) { encryptedLetter += 32; }
+			encryptedText += (char)encryptedLetter;
+		}
+		
+		return encryptedText;
+	}
+	
+	public static string DecryptASCII(string text, string key){
+		string decryptedText = "";
+		
+		for(int i = 0; i < text.Length; i++){
+			int textLetter = (int)text[i];
+			int keyLetter = (int)key[i % key.Length];
+			int decryptedLetter = (textLetter - keyLetter);
+			if(decryptedLetter < 0) { decryptedLetter += 127; }
+			decryptedText += (char)decryptedLetter;
+		}
+		
+		return decryptedText;
+	}
+}
